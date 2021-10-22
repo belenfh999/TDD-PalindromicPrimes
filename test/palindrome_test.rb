a7c01c4b5089629cf06c4584a7909b55e0d0a07d
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-22 14:04:10 +0200

    Added more tests for palindromes of 2 digits

--- test/palindrome_test.rb
+++ test/palindrome_test.rb
@@ -35,4 +35,28 @@ class PalindromeTest < Minitest::Test
     expected_output = false
     assert_equal(expected_output, @palindrome.palindrome?(input))
   end
+
+  def test_palindrome_11_returns_true
+    input = 11
+    expected_output = true
+    assert_equal(expected_output, @palindrome.palindrome?(input))
+  end
+
+  def test_palindrome_12_returns_false
+    input = 12
+    expected_output = false
+    assert_equal(expected_output, @palindrome.palindrome?(input))
+  end
+
+  def test_palindrome_22_returns_true
+    input = 22
+    expected_output = true
+    assert_equal(expected_output, @palindrome.palindrome?(input))
+  end
+
+  def test_palindrome_43_returns_false
+    input = 43
+    expected_output = false
+    assert_equal(expected_output, @palindrome.palindrome?(input))
+  end
 end

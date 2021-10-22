06fda85ad3ae211791cc9cb3edf2ba6bd955d861
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-22 13:23:33 +0200

    Added implementation for finding prime number

--- lib/prime.rb
+++ lib/prime.rb
@@ -3,6 +3,7 @@ module PrimeMod
     def prime?(number)
       is_prime = true
       return !is_prime if number < 2
+      (2...number).each { |possible_divisor| return !is_prime if (number % possible_divisor) == 0 }
       is_prime
     end
   end

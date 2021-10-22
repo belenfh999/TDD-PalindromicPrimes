58bb4f4a8abc77075c0572fdbe6cb81b6a80ae6d
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-22 10:11:44 +0200

    Adding failing test for prime? 4

--- test/prime_test.rb
+++ test/prime_test.rb
@@ -29,4 +29,10 @@ class PrimeTest < Minitest::Test
     expected_output = true
     assert_equal(expected_output, @prime.prime?(input))
   end
+
+  def test_prime_4_returns_false
+    input = 4
+    expected_output = false
+    assert_equal(expected_output, @prime.prime?(input))
+  end
 end

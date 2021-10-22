e000396f7438ada58496a190f4453de72c9f1d10
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-22 09:58:42 +0200

    Adding failing tests for primes less than 2

---
+++ test/prime_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require_relative '../lib/prime'
+class PrimeTest < Minitest::Test
+  include PrimeMod
+  def setup
+    @prime = Prime.new
+  end
+
+  def test_prime_minus_3_r_prime_false
+    input = -3
+    expected_output = false
+    assert_equal(expected_output, @prime.prime?(input))
+  end
+
+  def test_prime_0_returns_false
+    input = 0
+    expected_output = false
+    assert_equal(expected_output, @prime.prime?(input))
+  end
+end

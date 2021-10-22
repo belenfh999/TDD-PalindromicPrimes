9095f2fdfdf2fd50ae9e8c07a1ad5c88605166fd
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-22 13:28:41 +0200

    Final implementation refactor

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,9 +1,7 @@
 module PrimeMod
   class Prime
     def prime?(number)
-      is_prime = true
-      return !is_prime if number < 2
-      (2...number).none? { |possible_divisor| (number % possible_divisor).zero? }
+      number >= 2 && (2...number).none? { |possible_divisor| (number % possible_divisor).zero? }
     end
   end
 end

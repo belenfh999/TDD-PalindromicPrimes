6d882a39ae0add798d176f9e1c23d25ff0d7dead
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-22 14:25:20 +0200

    Added more tests

--- test/palindrome_prime_test.rb
+++ test/palindrome_prime_test.rb
@@ -6,9 +6,27 @@ class PalindromePrimeTest < Minitest::Test
     @palindrome_prime = PalindromePrime.new
   end
 
+  def test_palindrome_prime_10_returns_false
+    input = 10
+    expected_output = false
+    assert_equal(expected_output, @palindrome_prime.palindrome_prime?(input))
+  end
+
+  def test_palindrome_prime_22_returns_false
+    input = 22
+    expected_output = false
+    assert_equal(expected_output, @palindrome_prime.palindrome_prime?(input))
+  end
+
   def test_palindrome_prime_101_returns_true
     input = 101
     expected_output = true
+    assert_equal(expected_output, @palindrome_prime.palindrome_prime?(input))
+  end
+
+  def test_palindrome_prime_222_returns_false
+    input = 222
+    expected_output = false
     assert_equal(expected_output, @palindrome_prime.palindrome_prime?(input))
   end
 end
